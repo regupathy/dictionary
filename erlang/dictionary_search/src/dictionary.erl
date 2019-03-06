fed639677c24801d68e06560bf899629a75a7a7c
dharan <regupathy043@gmail.com>
2019-03-06 22:42:47 +0530

    created a method remove_duplicate/1 to avoid the duplicate entry in Dictionary

--- erlang/dictionary_search/src/dictionary.erl
+++ erlang/dictionary_search/src/dictionary.erl
@@ -10,7 +10,7 @@
 -author("regupathy").
 
 %% API
--export([add/2,search/2]).
+-export([add/2,search/2, remove_duplicate/1]).
 
 -type key() :: number() | string() | atom().
 
@@ -35,9 +35,7 @@
 -spec add(Word::string(),Dictionary::dictionary()) -> NewDictionary::dictionary().
 add(Word,Dictionary) ->
   case catch word_to_key(Word) of
-
     {throw,Error} -> io:format("Not able to add word : ~p bz of : ~p",[Word,Error]),Dictionary;
-
     Keys -> add(Keys,Dictionary,Word)
   end.
 
@@ -62,46 +60,39 @@ search(Number,Dictionary)when is_number(Number) ->
   end.
 
 search([Key|[]],SubTree,_Dict,Acc) ->
-
+%% No need to check the NestedTree
   case dict_take(Key,SubTree) of
-
     not_found -> []; %% Simply Ignore because of No Entry in Dictionary for Key
-
-    %% No need to check the NestedTree
     {match,{Key,[],_}} ->  []; %% Simply Ignore because of Empty word list in Dictionary for Key
-
     {match,{Key,Words,_}} -> zip([Words|Acc],[])
-
   end;
 
 search([Key|Rest],SubTree,Dict,Acc) ->
-
   case dict_take(Key,SubTree) of
-
     not_found ->  []; %% No Entry in Dictionary
-
     {match,{Key,[],[]}} -> []; %% Empty entry in Dictionary
-
     {match,{Key,[],NestedTree}} -> search(Rest,NestedTree,Dict,Acc); %% Incomplete Word
-
     {match,{Key,Words,[]}} -> search(Rest,Dict,Dict,[Words|Acc]); %% No further continuation
-
     {match,{Key,Words,NestedTree}} -> %% Has further continuation with set words
-
       search(Rest,Dict,Dict,[Words|Acc]) %% carry forward with word list and start search from original Dict
       ++
       search(Rest,NestedTree,Dict,Acc)  %% search will carry forward with Nested Tree and not considering the word list
-
   end.
 
 %%========================================================================
 %%                   Remove the Duplicate word in Dictionary
 %%========================================================================
--spec avoid_duplicate(Dictionary::dictionary()) -> NewDicitonary::dictionary().
-avoid_duplicate(Dictionary) -> ok.
+-spec remove_duplicate(Dictionary::dictionary()) -> NewDicitonary::dictionary().
+remove_duplicate(Dict) -> remove_duplicate(Dict,[]).
 
+remove_duplicate([],Dict) -> Dict;
+remove_duplicate([{Key,[],[]}|Rest],Acc) -> remove_duplicate(Rest,[{Key,[],[]}|Acc]);
+remove_duplicate([{Key,Words,[]}|Rest],Acc) -> remove_duplicate(Rest,[{Key,rm_dup(Words),[]}|Acc]);
+remove_duplicate([{Key,[],SubTree}|Rest],Acc) -> remove_duplicate(Rest,[{Key,[], remove_duplicate(SubTree,[])}|Acc]);
+remove_duplicate([{Key,Words,SubTree}|Rest],Acc) ->
+  remove_duplicate(Rest,[{Key,rm_dup(Words), remove_duplicate(SubTree,[])}|Acc]).
 
-
+rm_dup(List) -> sets:to_list(sets:from_list(List)).
 %%========================================================================
 %%                   Local conversion
 %%========================================================================
@@ -114,8 +105,6 @@ number_to_list_of_int(Number) -> number_to_list_of_int(Number,[]).
 number_to_list_of_int(0,Acc) -> Acc;
 number_to_list_of_int(N,Acc) -> number_to_list_of_int(N div 10,add_only_if(N rem 10 , Acc)).
 
-
-
 %% @doc
 %% convert word into keys
 %% Limitation :
@@ -123,7 +112,6 @@ number_to_list_of_int(N,Acc) -> number_to_list_of_int(N div 10,add_only_if(N rem
 
 -spec word_to_key(Word::string()) -> Key::key().
 word_to_key(Word) -> lists:map(fun(X) -> char_to_number(X) end,Word).
-
 
 %%========================================================================
 %%                   Local Functions
